8d8c4fa0675ff28b781b67dc31b49b8c7bf49257
Victor Akanbi <vakanbi971@west-mec.org>
2019-03-07 14:14:27 -0700

    First Commit
    Added variables and switch funtions

--- jpf_hanjie.js
+++ jpf_hanjie.js
@@ -60,8 +60,35 @@ function init() {
       document.getElementById("puzzleTitle").innerHTML = "Puzzle 1";
 
       //insert html code for the first puzzle table
-      document.getElementById("puzzle").innerHTML = drawPuzzle(puzzleHint, puzzleRating, puzzle1);
+      document.getElementById("puzzle").innerHTML = drawPuzzle(puzzle1Hint, puzzle1Rating, puzzle1);
 
+      //add event handlers for the puzzle buttons
+      var puzzleButtons = document.getElementsByClassName("puzzles");
+      for (var i = 0; i < puzzleButtons.length; i++) {
+            puzzleButtons[i].onclick = swapPuzzle;
+      }
+}
+
+function swapPuzzle(e) {
+
+      //retrieve the id of the clicked button
+      var puzzleID = e.target.id;
+
+      //retrieve the value of the clicked button
+      var puzzleTitle = e.target.value;
+      document.getElementById("puzzleTitle").innerHTML = puzzleTitle;
+
+      //display value based on the value of the puzzleID var
+      switch (puzzleID) {
+            case "puzzle1":
+                  document.getElementById("puzzle").innerHTML = drawPuzzle(puzzle1Hint, puzzle1Rating, puzzle1);
+                  break;
+            case "puzzle2":
+                  document.getElementById("puzzle").innerHTML = drawPuzzle(puzzle2Hint, puzzle2Rating, puzzle2);
+                  break;
+            default:
+                  break;
+      }
 }
 
 
