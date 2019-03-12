3ab25f1b958c5ee15f3932090227471c340b71d6
Victor Akanbi <vakanbi971@west-mec.org>
2019-03-12 14:49:08 -0700

    Final commit
    Ready for review

--- jpf_hanjie.js
+++ jpf_hanjie.js
@@ -72,44 +72,46 @@ function init() {
             puzzleButtons[i].onclick = swapPuzzle;
       }
       setupPuzzle();
+
+
+      document.addEventListener("mouseup", endBackground);
+
+      //add event listener to show solution button
+      document.getElementById("solve").addEventListener("click", function () {
+            //remove inline background color styles from eachc ell
+            for (var i = 0; i < puzzleCells.length; i++) {
+                  puzzleCells[i].style.backgroundColor = "";
+            }
+      })
 }
-
-document.addEventListener("mouseup", endBackground);
-
-//add event listener to show solution button
-document.getElementById("solve").addEventListener("click", function () {
-      //remove inline background color styles from eachc ell
-      for (var i = 0; i < puzzleCells.length; i++) {
-            puzzleCells[i].style.backgroundColor = "";
-      }
-})
-
 
 
 var cellBackground;
 
 function swapPuzzle(e) {
+      if (confirm("You will lose all of your work on the puzzle! Continue?")) {
 
-      //retrieve the id of the clicked button
-      var puzzleID = e.target.id;
+            //retrieve the id of the clicked button
+            var puzzleID = e.target.id;
 
-      //retrieve the value of the clicked button
-      var puzzleTitle = e.target.value;
-      document.getElementById("puzzleTitle").innerHTML = puzzleTitle;
+            //retrieve the value of the clicked button
+            var puzzleTitle = e.target.value;
+            document.getElementById("puzzleTitle").innerHTML = puzzleTitle;
 
-      //display value based on the value of the puzzleID var
-      switch (puzzleID) {
-            case "puzzle1":
-                  document.getElementById("puzzle").innerHTML = drawPuzzle(puzzle1Hint, puzzle1Rating, puzzle1);
-                  break;
-            case "puzzle2":
-                  document.getElementById("puzzle").innerHTML = drawPuzzle(puzzle2Hint, puzzle2Rating, puzzle2);
-                  break;
-            case "puzzle3":
-                  document.getElementById("puzzle").innerHTML = drawPuzzle(puzzle3Hint, puzzle3Rating, puzzle3);
-                  break;
+            //display value based on the value of the puzzleID var
+            switch (puzzleID) {
+                  case "puzzle1":
+                        document.getElementById("puzzle").innerHTML = drawPuzzle(puzzle1Hint, puzzle1Rating, puzzle1);
+                        break;
+                  case "puzzle2":
+                        document.getElementById("puzzle").innerHTML = drawPuzzle(puzzle2Hint, puzzle2Rating, puzzle2);
+                        break;
+                  case "puzzle3":
+                        document.getElementById("puzzle").innerHTML = drawPuzzle(puzzle3Hint, puzzle3Rating, puzzle3);
+                        break;
+            }
+            setupPuzzle();
       }
-      setupPuzzle();
 }
 
 function setupPuzzle() {
@@ -134,6 +136,35 @@ function setupPuzzle() {
                   if (filled[i].style.backgroundColor === "rgb(255, 255, 255)") {
                         filled[i].style.backgroundColor = "rgb(255, 211, 211)"
                   }
+            }
+            //display incorrect grey cells in red 
+            for (var i = 0; i < empty.length; i++) {
+                  if (empty[i].style.backgroundColor === "rgb(101, 101, 101)") {
+                        empty[i].style.backgroundColor = "rgb(255, 101, 101)"
+                  }
+            }
+            setTimeout(
+                  function () {
+                        for (var i = 0; i < puzzleCells.length; i++) {
+                              if (puzzleCells[i].style.backgroundColor === "rgb(255, 211, 211)") {
+                                    puzzleCells[i].style.backgroundColor = "rgb(255, 255, 255)";
+                              }
+                              if (puzzleCells[i].style.backgroundColor === "rgb(255, 101, 101)") {
+                                    puzzleCells[i].style.backgroundColor = "rgb(101, 101, 101)";
+                              }
+                        }
+                  }, 500);
+      });
+      document.getElementById("hanjieGrid").addEventListener("mouseup", function () {
+            var solved = true;
+            for (let i = 0; i < puzzleCells.length; i++) {
+                  if ((puzzleCells[i].className === "filled" && puzzleCells[i].style.backgroundColor !== "rgb(101, 101, 101)") || (puzzleCells[i].className === "empty" && puzzleCells[i].style.backgroundColor === "rgb(101, 101, 101)")) {
+                        solved = false;
+                        break;
+                  }
+            }
+            if (solved) {
+                  alert("You solved the PUZZLE!!!");
             }
       });
 }
